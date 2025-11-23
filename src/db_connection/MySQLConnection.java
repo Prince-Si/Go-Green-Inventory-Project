c55fff51e3591dedbc38a9d8fe3aa1f4f212c1fb
Prince Singh <136824628+Prince-Si@users.noreply.github.com>
2025-11-23 14:34:27 +0530

    Update MySQLConnection.java

--- src/db_connection/MySQLConnection.java
+++ src/db_connection/MySQLConnection.java
@@ -1,27 +1,24 @@
 package db_connection;
 
 import java.sql.Connection;
-
 import java.sql.DriverManager;
 
 public class MySQLConnection {
-	
-	private static String ConnURL = "jdbc:mysql://localhost:3306/smarketdb";
-	private static String username = "root";
-	private static String password = "#Root123";
-	
-	public static String forNameClassVar = "com.mysql.cj.jdbc.Driver";
-	
-	public static Connection DBConn() {
-		Connection con = null;
-		try {
-			con=DriverManager.getConnection(ConnURL,username,password);
-			}
-		catch(Exception e) {
-				System.out.println(e);
-				e.printStackTrace();
-			}
-		return con;
-	}
-	
+    
+    private static String ConnURL = "jdbc:mysql://localhost:3306/YOUR_DATABASE_NAME";
+    private static String username = "YOUR_DB_USERNAME";
+    private static String password = "YOUR_DB_PASSWORD";
+    
+    private static String forNameClassVar = "com.mysql.cj.jdbc.Driver";
+    
+    public static Connection DBConn() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(ConnURL, username, password);
+        } catch(Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return con;
+    }
 }
